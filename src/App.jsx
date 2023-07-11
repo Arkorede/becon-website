27ee9468f064c4add2280d6f46eecc079b766f99
arkorede <israelashaolu2000@gmail.com>
2023-07-11 16:35:35 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
   return (
     <div className="max-[410px]:w-[410px]">
       {/* HEADER  SECTION */}
-      <header className="relative py-4 border border-[red]">
+      <header className="relative py-4">
         <nav className="flex items-center justify-center justify-between px-16 md:px-32">
           <div className="flex items-center justify-center gap-4">
             <img
